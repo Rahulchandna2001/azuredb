b876b0134788b9160e783166816b7bfa064d54f0
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-10-08 21:43:56 -0400

    Update update_companydb_schema.sql

--- update_companydb_schema.sql
+++ update_companydb_schema.sql
@@ -1,4 +1,4 @@
--- Create departments table
+-- Create department table
 CREATE TABLE IF NOT EXISTS departments (
     department_id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(255) NOT NULL,
