30faafa34b5edf17947242a9f6f95bbad2af559a
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-10-08 20:44:41 -0400

    Update automate_schema_changes.py

--- automate_schema_changes.py
+++ automate_schema_changes.py
@@ -1,19 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
 
-"""
 # Create the database connection directly
-#connection = mysql.connector.connect(
-    host='db-rcha5267.mysql.database.azure.com',  
-    user='rootdb',  
-    password='Secret55',  
-    database='companydb'
+  connection = mysql.connector.connect(
+    host=${{ secrets.DB_HOST }},  
+    user=${{ secrets.DB_ADMIN_USER }},  
+    password=${{ secrets.DB_PASSWORD }},  
+    database=${{ secrets.DB_NAME }}
 )
 
 
 # Create a cursor object to execute SQL queries
 cursor = connection.cursor() 
-"""
+
 # Path to the SQL file
 script_path = 'update_projects_schema.sql'
 
@@ -21,16 +20,13 @@ script_path = 'update_projects_schema.sql'
 with open(script_path, 'r') as file:
     sql_script = file.read()
 
- 
-
 # Execute each SQL statement individually (split by semicolon)
 for statement in sql_script.split(';'):
     if statement.strip():  # Execute only non-empty statements
-       """ 
        cursor.execute(statement)
 
 connection.commit()  # Commit all changes at once
 cursor.close()  # Close the cursor
 connection.close()  # Close the connection
-"""
+
 print("Execution completed successfully")
